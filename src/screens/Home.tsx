96c8a7cf9fb0c492f39b716da26d021a0ab21667
Jefferson Mantovani <jgsmantovani@gmail.com>
2023-11-24 23:02:57 +0000

    feat: create Group list component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,35 @@
+import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { VStack, Text } from "native-base";
+import { VStack, Text, HStack, FlatList } from "native-base";
+import { ItemClick } from "native-base/lib/typescript/components/composites/Typeahead/useTypeahead/types";
+import { useState } from "react";
 
 export function Home () {
+  const [selectedGroup, setSelectedGroup] = useState('costa')
+  const [groups, setGroups] = useState(['costas', 'biceps', 'triceps', 'ombro'])
+  
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={selectedGroup === item} 
+            onPress={() => setSelectedGroup(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
+
+
     </VStack>
   )
 }
