b486b4ba3fa07c7bb826d20757fc669672a23136
Jefferson Mantovani <jgsmantovani@gmail.com>
2023-11-24 23:19:26 +0000

    feat: create Exercises list in Home screen

---
+++ src/components/ExerciseCard.tsx
@@ -0,0 +1,40 @@
+import { HStack, Heading, Icon, Image, Text, VStack } from "native-base";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { Entypo } from '@expo/vector-icons'
+
+type ExerciseCardProps = TouchableOpacityProps & {
+
+}
+
+export function ExerciseCard ({ ...rest }: ExerciseCardProps) {
+  return (
+    <TouchableOpacity
+      {...rest}
+    >
+      <HStack bg="gray.500" alignItems="center" p={2} pr={4} rounded="md" mb={3}>
+        <Image 
+          source={{ uri: "https://academiaironhealth.com.br/wp-content/uploads/revslider/3-dicas-para-estruturar-melhor-seu-treino/treino-ABC.jpg" }}
+          alt="Imagem do exercícios"
+          w={16}
+          h={16}
+          mr={4}
+          rounded="md"
+        />
+        <VStack flex={1}>
+          <Heading
+            color="white"
+            fontSize="lg"
+          >
+            Remada Unilateral
+          </Heading>
+          <Text fontSize="sm" color="gray.200" mt={1} numberOfLines={2}>
+            3 séries x 12 repetições
+          </Text>
+        </VStack>
+
+        <Icon as={Entypo} name="chevron-thin-right" color="gray.300" />
+      </HStack>
+
+    </TouchableOpacity>
+  )
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,14 @@
+import { ExerciseCard } from "@components/ExerciseCard";
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { VStack, Text, HStack, FlatList } from "native-base";
+import { VStack, Text, HStack, FlatList, Heading } from "native-base";
 import { ItemClick } from "native-base/lib/typescript/components/composites/Typeahead/useTypeahead/types";
 import { useState } from "react";
 
 export function Home () {
   const [selectedGroup, setSelectedGroup] = useState('costa')
   const [groups, setGroups] = useState(['costas', 'biceps', 'triceps', 'ombro'])
+  const [exercises, setExercises] = useState(['Puxada Frontal', 'Remada Unilateral', 'Remada Curva', 'Levantamento Terra'])
   
   return (
     <VStack flex={1}>
@@ -18,7 +20,7 @@ export function Home () {
         renderItem={({ item }) => (
           <Group
             name={item}
-            isActive={selectedGroup === item} 
+            isActive={selectedGroup.toUpperCase() === item.toUpperCase()} 
             onPress={() => setSelectedGroup(item)}
           />
         )}
@@ -29,7 +31,25 @@ export function Home () {
         maxH={10}
       />
 
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+            <Heading color="gray.200" fontSize="md">
+              Exercícios
+            </Heading>
 
+            <Text color="gray.200" fontSize="md">{exercises.length}</Text>
+        </HStack>
+
+        <FlatList 
+          data={exercises}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <ExerciseCard />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 8 }}
+        />
+      </VStack>
     </VStack>
   )
 }
