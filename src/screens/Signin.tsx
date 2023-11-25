2fd9a3a6bf2e7b2f33df00f214f8463943630710
Jefferson Mantovani <jgsmantovani@gmail.com>
2023-11-25 17:05:55 +0000

    refactor: use react hook form in signin screen

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,16 +1,57 @@
-import { Center, Image, Text, VStack, Heading, ScrollView } from "native-base";
+import { Center, Image, Text, VStack, Heading, ScrollView, useToast } from "native-base";
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImage from '@assets/background.png'
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 import { useNavigation } from "@react-navigation/native";
 import { AuthNavigatorRoutesprops } from '@routes/auth.routes'
+import { SigninDTO } from "@dtos/signin.dto";
+import { Controller, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { signinSchema } from "@utils/validations/signin.schema";
+import { useState } from "react";
+import { useAuth } from "@hooks/useAuth";
+import { AppError } from "@utils/app.error";
+import { Loading } from "@components/Loading";
 
 export function Signin () {
   const { navigate } = useNavigation<AuthNavigatorRoutesprops>()
 
-  function handleNewAccount () {
+  const toast = useToast()
+
+  const { signin, isLoadingUserData } = useAuth()
+
+  const { control, handleSubmit, formState: { errors, isValid } } = useForm<SigninDTO>({
+    resolver: yupResolver(signinSchema)
+  })
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  async function handleSignin ({ email, password }: SigninDTO) {
+    setIsLoading(true)
+    try {
+      await signin(email, password)
+      
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const errorMessage = isAppError ? error.message : 'Não foi possível logar'
+      
+      toast.show({
+        title: errorMessage,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+
+      setIsLoading(false)
+    }
+  }
+
+  function goToSignup () {
     navigate("signup")
+  }
+
+  if (isLoadingUserData) {
+    return <Loading />
   }
 
   return (
@@ -49,17 +90,41 @@ export function Signin () {
             Acesse sua conta
           </Heading>
 
-          <Input 
-            placeholder="Email"
-            keyboardType="email-address"
-            autoCapitalize="none"
-          />
-          <Input 
-            placeholder="Senha"
-            secureTextEntry 
+          <Controller 
+            control={control}
+            name="email"
+            render={({ field: { onChange, value } }) => (
+              <Input 
+              placeholder="Email"
+              keyboardType="email-address"
+              autoCapitalize="none"
+              value={value}
+              onChangeText={onChange}
+              errorMessage={errors.email?.message as string}
+            />
+            )}
           />
 
-          <Button title="Acessar" />
+          <Controller 
+              control={control} 
+              name="password"
+              render={({ field: { onChange, value } }) => (
+                <Input 
+                  placeholder="Senha"
+                  secureTextEntry
+                  value={value}
+                  onChangeText={onChange}
+                  errorMessage={errors.password?.message as string}
+                />
+              )} 
+            />
+
+          <Button 
+            title="Acessar" 
+            onPress={handleSubmit(handleSignin)}
+            disabled={isLoading || !isValid}
+            isLoading={isLoading}
+          />
         </Center>
 
         <Center mt={24}>
@@ -74,7 +139,7 @@ export function Signin () {
           <Button 
             title="Criar conta"
             variant="outline"
-            onPress={handleNewAccount}
+            onPress={goToSignup}
           />
           </Center>
       </VStack>
