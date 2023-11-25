2eb4f73dbe6e8ef23070faf5751f68459f103388
Jefferson Mantovani <jgsmantovani@gmail.com>
2023-11-25 17:06:43 +0000

    feat: verify if user is logged to allow access to private routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,25 @@
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
-import { AppRoutes } from "./app.routes";
 import { Box, useTheme } from "native-base";
 import { AuthRoutes } from "./auth.routes";
+import { useAuth } from "@hooks/useAuth";
+import { AppRoutes } from "./app.routes";
 
 export function Routes () {
   const { colors } = useTheme()
+
+  const { user } = useAuth()
+
   const theme = DefaultTheme;
   theme.colors.background= colors.gray['700']
 
   return (
     <Box flex={1} bg="gray.700">
       <NavigationContainer theme={theme}>
-        {/* <AppRoutes /> */}
-        <AuthRoutes />
+        {
+          user?.id ? 
+            <AppRoutes /> : 
+            <AuthRoutes />
+        }
       </NavigationContainer>
     </Box>
   )
